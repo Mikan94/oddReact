3804c52f931b42395b46b2644cd3c14910364a47
mikan94 <hallo.minkenberg@gmail.com>
2022-03-07 01:51:49 +0100

    🚧 condition to display different kind of games

--- src/Services/Database/getGamesData.js
+++ src/Services/Database/getGamesData.js
@@ -29,49 +29,55 @@ function GetGamesData() {
   return (
     <>
       <h1>Games</h1>
+      <div>{state.currentRound}</div>
       <button onClick={() => navigate("/rounds")}>rounds</button>
-      <div>
-        {gamesData
-          .filter((games) => state.item == games.round)
-          .map((game) =>
-            oddsData
-              .filter((item) => game.gameID == item.oddID)
-              .map((odd) => {
-                return (
-                  <>
-                    <div>{game.gameID}</div>
-                    <div>{game.round}</div>
-                    <div>{new Date(game.date).toLocaleString()}</div>
-                    <div>{game.teamHome}</div>
-                    <div>{game.teamAway}</div>
-                    <div>{game.scoreHome}</div>
-                    <div>{game.scoreAway}</div>
-                    <div>{odd.oddID}</div>
-                    <div>{odd.oddHome}</div>
-                    <div>{odd.oddDraw}</div>
-                    <div>{odd.oddAway}</div>
-                  </>
-                );
-              })
-          )}
-      </div>
-      <div>
-        {gamesData
-          .filter((games) => state.item == games.round)
-          .map((game) => {
-            return (
-              <>
-                <div>{game.gameID}</div>
-                <div>{game.round}</div>
-                <div>{new Date(game.date).toLocaleString()}</div>
-                <div>{game.teamHome}</div>
-                <div>{game.teamAway}</div>
-                <div>{game.scoreHome}</div>
-                <div>{game.scoreAway}</div>
-              </>
-            );
-          })}
-      </div>
+      {state.item == state.currentRound ? (
+        <div>
+          true
+          {gamesData
+            .filter((games) => state.item == games.round)
+            .map((game) =>
+              oddsData
+                .filter((item) => game.gameID == item.oddID)
+                .map((odd) => {
+                  return (
+                    <>
+                      <div>{game.gameID}</div>
+                      <div>{game.round}</div>
+                      <div>{new Date(game.date).toLocaleString()}</div>
+                      <div>{game.teamHome}</div>
+                      <div>{game.teamAway}</div>
+                      <div>{game.scoreHome}</div>
+                      <div>{game.scoreAway}</div>
+                      <div>{odd.oddID}</div>
+                      <div>{odd.oddHome}</div>
+                      <div>{odd.oddDraw}</div>
+                      <div>{odd.oddAway}</div>
+                    </>
+                  );
+                })
+            )}
+        </div>
+      ) : (
+        <div>
+          false
+          {gamesData
+            .filter((games) => state.item == games.round)
+            .map((game) => {
+              return (
+                <>
+                  <div>{game.gameID}</div>
+                  <div>{game.round}</div>
+                  <div>{new Date(game.date).toLocaleString()}</div>
+                  <div>{game.teamHome}</div>
+                  <div>{game.teamAway}</div>
+                  <div>{game.scoreHome}</div>
+                  <div>{game.scoreAway}</div>
+                </>
+              );
+            })}
+        </div>
+      )}
     </>
   );
 }
--- src/Services/Database/getRounds.js
+++ src/Services/Database/getRounds.js
@@ -5,16 +5,30 @@ import { useNavigate } from "react-router-dom";
 
 function GetRounds(props) {
   const [rounds, setRounds] = useState([]);
+  const [currentRound, setCurrentRound] = useState([]);
 
   const apiHost = "v3.football.api-sports.io";
-  const apiUrlGames = `https://${apiHost}/fixtures/rounds`;
+  const apiUrlRounds = `https://${apiHost}/fixtures/rounds`;
 
   const toParamString = (obj) => new URLSearchParams(obj).toString();
 
   const apiKey = "b8815df003f549ac6fd8db7f3e27c045";
 
   const getRoundsData = ({ season, league }) => {
-    const url = `${apiUrlGames}?${toParamString({ season, league })}`;
+    const url = `${apiUrlRounds}?${toParamString({ season, league })}`;
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "v3.football.api-sports.io",
+        "x-apisports-key": apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => data.response);
+  };
+
+  const getRoundCurrent = ({ season, league, current }) => {
+    const url = `${apiUrlRounds}?${toParamString({ season, league, current })}`;
     return fetch(url, {
       method: "GET",
       headers: {
@@ -33,11 +47,20 @@ function GetRounds(props) {
     }).then((data) => {
       setRounds(data);
     });
+
+    getRoundCurrent({
+      season: 2021,
+      league: 78,
+      current: true,
+    }).then((data) => {
+      setCurrentRound(data);
+    });
   };
 
   useEffect(() => {
     getDataAPI();
     console.log(rounds);
+    console.log(currentRound);
   }, []);
 
   const navigate = useNavigate();
@@ -45,10 +68,14 @@ function GetRounds(props) {
   return (
     <>
       <div>
-        Rounds
+        Rounds {currentRound}
         <div>
           {rounds.map((item) => (
-            <button onClick={() => navigate("/games", { state: { item } })}>
+            <button
+              onClick={() =>
+                navigate("/games", { state: { item, currentRound } })
+              }
+            >
               <Card roundID={item} />
             </button>
           ))}
