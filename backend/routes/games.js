18148f9de10c7aed8306335b28f45275f055d0e4
mikan94 <hallo.minkenberg@gmail.com>
2022-03-12 23:37:27 +0100

    🚧 added new endpoint for game update

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -32,4 +32,29 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.setMaxListeners(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Game.findById(req.params.id)
+    .then((game) => res.json(game))
+    .catch((err) => res.status.json("Error" + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Game.findById(req.params.id)
+    .then((game) => {
+      game.gameID = req.body.gameID;
+      game.round = req.body.round;
+      game.date = req.body.date;
+      game.teamHome = req.body.teamHome;
+      game.teamAway = req.body.teamAway;
+      game.scoreHome = req.body.scoreHome;
+      game.scoreAway = req.body.scoreAway;
+
+      game
+        .save()
+        .then(() => res.json("Score updated"))
+        .catch((err) => res.status(400).json("Error" + err));
+    })
+    .catch((err) => res.status(400).json("Error" + err));
+});
+
 module.exports = router;
